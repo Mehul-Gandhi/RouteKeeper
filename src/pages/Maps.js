03ffb8eae58cca48e6dfac1b711b7c7ea3e3a48b
Mehul-Gandhi <gandhi@berkeley.edu>
2023-07-03 15:05:45 -0700

    Add place info popup

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -7,15 +7,14 @@ import {
   DirectionsService,
   DirectionsRenderer,
   Autocomplete,
-  Marker
+  Marker,
+  InfoWindow
 } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '400px',
   height: '400px',
 };
-
-
 
 export default function Maps() {
   const center = {
@@ -24,17 +23,19 @@ export default function Maps() {
   };
 
   const markerPositions = [
-    { lat: 37.7749, lng: -122.4194 }, // Marker 1
+    { lat: 37.7749, lng: -122.4194}, // Marker 1
     { lat: 37.772, lng: -122.41 }, // Marker 2
     { lat: 37.775, lng: -122.415 }, // Marker 3
+    { lat: 3.1494729, lng: 101.6539734,  name: "Hoshun", category: "Restaurant"},
+    { lat: 3.1664583, lng: 101.6523213, name: "Urban Retreat Onsen", category: "Spa"},
+    { lat: 3.1441858, lng: 101.6985414, name: "Shhhbuuulee", category: "Restaurant"},
   ];
 
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
   const [travelMode, setTravelMode] = useState('DRIVING');
   const [response, setResponse] = useState(null);
-
-  
+  const [selectedPlace, setSelectedPlace] = useState(null);
 
   const travelModes = ['DRIVING', 'WALKING', 'BICYCLING', 'TRANSIT'];
 
@@ -57,41 +58,45 @@ export default function Maps() {
     );
   };
 
+  const handleMarkerClick = (place) => {
+    setSelectedPlace(place);
+  };
+
   return (
     <div>
-        <div>
-      <Autocomplete
-        onLoad={(autocomplete) => {
-          autocomplete.setFields(['formatted_address']);
-          autocomplete.addListener('place_changed', () => {
-            const place = autocomplete.getPlace();
-            setOrigin(place.formatted_address);
-          });
-        }}
-      >
-        <input
-          type="text"
-          placeholder="From"
-          value={origin}
-          onChange={(e) => setOrigin(e.target.value)}
-        />
-      </Autocomplete>
-      <Autocomplete
-        onLoad={(autocomplete) => {
-          autocomplete.setFields(['formatted_address']);
-          autocomplete.addListener('place_changed', () => {
-            const place = autocomplete.getPlace();
-            setDestination(place.formatted_address);
-          });
-        }}
-      >
-        <input
-          type="text"
-          placeholder="To"
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-        />
-      </Autocomplete>
+      <div>
+        <Autocomplete
+          onLoad={(autocomplete) => {
+            autocomplete.setFields(['formatted_address']);
+            autocomplete.addListener('place_changed', () => {
+              const place = autocomplete.getPlace();
+              setOrigin(place.formatted_address);
+            });
+          }}
+        >
+          <input
+            type="text"
+            placeholder="From"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+          />
+        </Autocomplete>
+        <Autocomplete
+          onLoad={(autocomplete) => {
+            autocomplete.setFields(['formatted_address']);
+            autocomplete.addListener('place_changed', () => {
+              const place = autocomplete.getPlace();
+              setDestination(place.formatted_address);
+            });
+          }}
+        >
+          <input
+            type="text"
+            placeholder="To"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+        </Autocomplete>
       </div>
       <select value={travelMode} onChange={(e) => setTravelMode(e.target.value)}>
         {travelModes.map((mode) => (
@@ -109,9 +114,28 @@ export default function Maps() {
       )}
 
         <GoogleMap mapContainerClassName="map-container" center={center} zoom={10}>
-         {markerPositions.map((position, index) => (
-          <Marker key={index} position={position} />
-        ))}
+          {markerPositions.map((position, index) => (
+            <Marker
+              key={index}
+              position={position}
+              onClick={() => handleMarkerClick(position)}
+            />
+          ))}
+
+          {selectedPlace && (
+            <InfoWindow
+              position={selectedPlace}
+              onCloseClick={() => setSelectedPlace(null)}
+            >
+              <div>
+                <h3>Place Information</h3>
+                <p>Name: {selectedPlace.name} </p>
+                <p>Category: {selectedPlace.category} </p>
+                <p>Latitude: {selectedPlace.lat}</p>
+                <p>Longitude: {selectedPlace.lng}</p>
+              </div>
+            </InfoWindow>
+          )}
 
           {response && <DirectionsRenderer directions={response} />}
         </GoogleMap>
